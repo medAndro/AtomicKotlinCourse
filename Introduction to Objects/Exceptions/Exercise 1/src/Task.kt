80ff4a8af2182fabfc260365594dfb3dc54abf0a
medAndro <med4ndro@gmail.com>
2025-02-10 01:42:45 +0900

    study: 아톰 23 예외

--- Introduction to Objects/Exceptions/Exercise 1/src/Task.kt
+++ Introduction to Objects/Exceptions/Exercise 1/src/Task.kt
@@ -2,7 +2,14 @@
 package exceptionsExercise1
 
 fun main() {
-  println("Can't be converted to double: ???")
+  println("Can't be converted to double: 1,2")
 
-  // TODO
+  val nums = listOf<String>("12", "1.2", "1,2", "1.2e0", "1.2e1",
+    "1.2e2", "1.2e3", "1.2e10", "12.3e10",
+    "1.2e-1", "1.2e-10")
+
+  for (num in nums) {
+    if ("," !in num)
+      println(num.toDouble())
+  }
 }
